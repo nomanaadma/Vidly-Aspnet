0f63df64873762f0b76c5c9d447df5c499e1b2e6
Noman Shoukat <nomanaadma@gmail.com>
2024-09-10 22:27:34 +0500

    project setup

---
+++ Vidly.Application/ApplicationServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Vidly.Application.Data;
+
+namespace Vidly.Application;
+
+public static class ApplicationServiceCollectionExtensions
+{
+	public static IServiceCollection AddApplication(this IServiceCollection services)
+	{
+		return services;
+	}
+	
+	public static IServiceCollection AddDatabase(this IServiceCollection services, 
+		string? connectionString)
+	{
+		services.AddDbContext<DatabaseContext>(optionBuilder =>
+		{
+			optionBuilder.UseNpgsql(connectionString);
+		});
+		return services;
+	}
+	
+}
---
+++ Vidly.Application/Data/DatabaseContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Vidly.Application.Data;
+
+public class DatabaseContext(DbContextOptions<DatabaseContext> options) : DbContext(options)
+{
+	
+}
